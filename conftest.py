0f0498df1325e1097fbf296275a2b2bf7a7bdfc6
Michael Zhang <me@michaelz.xyz>
2016-07-06 01:23:34 -0500

    Add basic tests.

---
+++ conftest.py
@@ -0,0 +1,56 @@
+import pytest
+
+from api.app import app as ctf_app
+from api.config import options
+from api.api.models import db as ctf_db
+
+@pytest.fixture(scope="session")
+def app(request):
+	app = ctf_app
+	app.config.from_object(options)
+	app.config["TESTING"] = True
+
+	ctx = app.test_request_context()
+	ctx.push()
+
+	def teardown():
+		ctx.pop()
+
+	request.addfinalizer(teardown)
+	return app
+
+@pytest.fixture(scope="session")
+def client(app):
+	return app.test_client()
+
+@pytest.fixture(scope="class")
+def db(request, app):
+	app_db.reflect()
+	app_db.drop_all()
+
+	app_db.create_all()
+
+	def teardown():
+		app_db.session.close()
+		app_db.drop_all()
+
+	request.addfinalizer(teardown)
+	return app_db
+
+@pytest.fixture(scope="class")
+def session(request, db):
+	connection = db.engine.connect()
+	transaction = connection.begin()
+
+	options = dict(bind=connection, binds={})
+	session = db.create_scoped_session(options=options)
+
+	db.session = session
+
+	def teardown():
+		transaction.rollback()
+		connection.close()
+		session.remove()
+
+	request.addfinalizer(teardown)
+	return session
--- tests/test_general.py
+++ tests/test_general.py
@@ -1,2 +1,10 @@
+import json
+
 class TestGeneral():
-	
+	def test_api_is_running(self, client):
+		response = json.loads(client.get("/api").data)
+		assert response["success"] == 1
+
+	def test_requires_setup(self, client):
+		response = json.loads(client.get("/api/user/status").data)
+		assert "redirect" in response and response["redirect"] == "/setup"
